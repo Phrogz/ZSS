e586eaff1f4e4ef267cf5b45938612092d5143f0
Gavin Kistner <gavin@phrogz.net>
2018-07-28 14:28:20 -0600

    Refactor to take value lookups and handlers to resolve values.
    * Change new() signature to allow setting up lookups and handlers before parsing.
    * Value lookups resolve specific value strings to predefined values.
    * Handler lookups resolve values like `func-name(a, b, c)` via custom functions.
    * Added inline comments to functions.

--- test.lua
+++ test.lua
@@ -4,30 +4,38 @@ p(z)
 
 ZSS = require 'zss'
 
+local function color(a,b,c)
+	print('COLOR!',a,b,c)
+	return 'COLOR'
+end
+
 -- parse raw CSS from a string
-rules = ZSS:new [[
-	@font-face { font-family:'main'; src:'DINPro.otf';  }
-	@font-face { font-family:'bold'; src:'DINPro-Bold.otf' }
+rules = ZSS:new{
+	values   = { none=false, ['false']=false, ['true']=true },
+	handlers = { color=color, rgba=color },
+	basecss  = [[
+		@font-face { font-family:'main'; src:'DINPro.otf';  }
+		@font-face { font-family:'bold'; src:'DINPro-Bold.otf' }
 
-  /* Make things stand out as ugly, to encourage adding necessary data */
+	  /* Make things stand out as ugly, to encourage adding necessary data */
+		*    { fill:none; stroke:'magenta';
+		       stroke-width:1; font:'main' }
 
-	*    { fill:false; stroke:'magenta';
-	       stroke-width:1; font:'main' }
+		text { font:'main'; fill:color('white'); stroke:none; size:12; opacity:0.5 }
 
-	text { font:'main'; fill:'white'; stroke:false; size:12; opacity:0.5 }
+		text.detection { size:8; fill:green }
+		.detection { fill:color('#ffff0033'); stroke:'yellow'; stroke-width:2 }
 
-	text.detection { size:8; fill:green }
-	.detection { fill:'#ffff0033'; stroke:'yellow'; stroke-width:2 }
+		.danger { fill:rgba(2,0,0); size:18; effect:glow(yellow,3) }
+		.pedestrian { fill:purple; fill-opacity:0.3 }
+		.child      { fill-opacity:0.8 }
 
-	.danger { fill:rgba(2,0,0); size:18; effect:glow(yellow,3) }
-	.pedestrian { fill:purple; fill-opacity:0.3 }
-	.child      { fill-opacity:0.8 }
-
-	*[ttc<1.5]  { effect:flash(0.2) }
-	bbox { fill:false; stroke-opacity:0.2 }
-	*[speed] { speed:true }
-	#accel { stroke:color('green') }
-]]
+		*[ttc<1.5]  { effect:flash(0.2) }
+		bbox { fill:none; stroke-opacity:0.2 }
+		*[speed] { speed:true }
+		#accel { stroke:color('green') }
+	]]
+}
 
 -- add a second document (can also load from a file)
 rules:add [[
@@ -36,21 +44,5 @@ rules:add [[
 	text { fill:orange }
 ]]
 
-p( rules:match{ } )
-p( rules:match{ id='accl', type='text', tags={danger=1,debug=1}, data={speed=-5} } )
-
-require 'os'
-local count = 1000000
-local clock = os.clock
-local start = clock()
-for i=1,count do
-	rules:match{ id='accl', type='text', tags={danger=1,debug=1}, data={speed=-5} }
-end
-print( clock() - start )
-
-start = clock()
-for i=1,count do
-	rules:match('text#accl.danger.debug[speed='..math.random(100)..']')
-end
-print( clock() - start )
-
+p( rules:match'line.child.counted' )
+-- p( rules:match{ id='accl', type='text', tags={danger=1,debug=1}, data={speed=5} } )
--- zss.lua
+++ zss.lua
@@ -1,34 +1,83 @@
 local ZSS = {}
 ZSS.__index = ZSS
 
-function ZSS:new(...)
-	local zss = setmetatable({rules={}, directives={}, computed={}},ZSS)
-	for _,css in ipairs{...} do zss:add(css) end
+-- ZSS:new{
+--   handlers = {color=processColor, url=processURL },
+--   values    = {none=false, ['true']=true, ['false']=false, transparent=processColor(0,0,0,0) },
+--   basecss   = [[...css code...]],
+--   files     = {'a.css', 'b.css'},
+-- }
+function ZSS:new(opts)
+	local zss = {
+		rules     = {}, -- array of rule tables, sorted by rank (specificity)
+		atrules   = {}, -- array of @font-face et. al, in document order
+		_computed = {}, -- cached element signatures mapped to computed declarations
+		_handlers = {}, -- function names (e.g. "url") mapped to function to process arguments into value
+		_values   = {}, -- value literal strings mapped to equivalent values (e.g. "none"=false)
+	}
+	setmetatable(zss,ZSS)
+	if opts.handlers then zss:handlers(opts.handlers)        end
+	if opts.values   then zss:values(opts.values)            end
+	if opts.basecss  then zss:add(opts.basecss)              end
+	if opts.files    then zss:load(table.unpack(opts.files)) end
 	return zss
 end
 
-function ZSS:load(filename)
-	local file = io.open(filename)
-	if file then
-		self:add(file:read('*all'))
-		file:close()
+-- Usage: myZSS:load( 'file1.css', 'file2.css', ... )
+function ZSS:load(...)
+	for _,file in ipairs{...} do
+		local file = io.open(filename)
+		if file then
+			self:add(file:read('*all'))
+			file:close()
+		else
+			error("Could not load CSS file '"..file.."'.")
+		end
 	end
 	return self
 end
 
-local function value(str)
-	local result = tonumber(str)
-	if not result then
-		str = str:gsub("^'(.-)'$", '%1'):gsub('^"(.-)"$', '%1')
-		if     str=='true'  then result=true
-		elseif str=='false' then result=false
-		else                     result=str
+-- Usage: myZSS:handlers{ color=processColor, url=processURL }
+function ZSS:handlers(handlers)
+	for k,f in pairs(handlers) do self._handlers[k]=f end
+	return self
+end
+
+-- Usage: myZSS:values{ none=false, false=false, transparent=Color(0,0,0,0) }
+function ZSS:values(valuemapping)
+	for k,v in pairs(valuemapping) do self._values[k]=v end
+	return self
+end
+
+-- Resolve values in declarations based on values and handlers
+function ZSS:eval(str)
+	local result = self._values[str]
+	if result==nil then result = tonumber(str) end
+	if result==nil then
+		result = str:match('^"(.-)"$') or str:match("^'(.-)'$")
+		if result==nil then
+			local func, params = str:match('^([%a_][%w_-]*)%(%s*(.-)%s*%)$')
+			if func then
+				local p={}
+				for param in params:gmatch('%s*([^,]+),?') do
+					table.insert(p,self:eval(param:gsub('%s+$','')))
+				end
+				if self._handlers[func] then
+					result = self._handlers[func](table.unpack(p))
+				else
+					result = { func=func, params=p }
+				end
+			else
+				result = str
+			end
 		end
 	end
 	return result
 end
 
-function ZSS.parse_selector(selector_str, from_data)
+-- Convert a selector string into its component pieces;
+-- from_data=true parses an element descriptor (skips some steps)
+function ZSS:parse_selector(selector_str, from_data)
 	if selector_str:match('^@[%a_][%w_-]*$') then
 		return {directive=selector_str}
 	else
@@ -44,9 +93,7 @@ function ZSS.parse_selector(selector_str, from_data)
 				selector.tags[#selector.tags+1] = name
 			end
 		end
-		if not from_data then
-			table.sort(selector.tags)
-		end
+		if not from_data then table.sort(selector.tags) end
 
 		if not from_data then
 			for name in selector_str:gmatch('%[%s*([%a_][%w_-]*)%s*%]') do
@@ -58,37 +105,36 @@ function ZSS.parse_selector(selector_str, from_data)
 		for name, op, val in selector_str:gmatch('%[%s*([%a_][%w_-]*)%s*([<=>])%s*(.-)%s*%]') do
 			if from_data then
 				if op=='=' then
-					selector.data[name] = value(val)
+					selector.data[name] = self:eval(val)
 				end
 			else
-				selector.data[name] = { op=op, value=value(val) }
+				selector.data[name] = { op=op, value=self:eval(val) }
 				selector.data[#selector.data+1] = name
 			end
 		end
-		if not from_data then
-			table.sort(selector.data)
-		end
+		if not from_data then table.sort(selector.data) end
 
 		return selector
 	end
 end
 
+-- Add a block of raw CSS rules (as a single string) to the style sheet
 function ZSS:add(css)
 	for rule_str in css:gsub('/%*.-%*/',''):gmatch('[^%s].-}') do
 		-- Convert declarations into a table mapping property to value
 		local decl_str = rule_str:match('{%s*(.-)%s*}')
 		local declarations = {}
 		for key,val in decl_str:gmatch('([^%s]+)%s*:%s*([^;}]+)') do
-			declarations[key] = value(val)
+			declarations[key] = self:eval(val)
 		end
 
 		-- Create a unique rule for each selector in the rule
 		local selectors_str = rule_str:match('(.-)%s*{')
 		for selector_str in selectors_str:gmatch('%s*([^,]+)') do
-			local selector = ZSS.parse_selector(selector_str:match "^%s*(.-)%s*$")
+			local selector = self:parse_selector(selector_str:match "^%s*(.-)%s*$")
 			if selector.directive then
 				selector.declarations = declarations
-				table.insert(self.directives, selector)
+				table.insert(self.atrules, selector)
 			else
 				selector.rank = {
 					selector.id and 1 or 0,
@@ -100,12 +146,9 @@ function ZSS:add(css)
 			end
 		end
 	end
-	self.computed = {}
-	self:sortrules()
-	return self
-end
+	self._computed = {}
 
-function ZSS:sortrules()
+	-- sort the rules by rank
 	table.sort(self.rules, function(r1, r2)
 		r1,r2 = r1.selector.rank,r2.selector.rank
 		if     r1[1]<r2[1] then return true elseif r1[1]>r2[1] then return false
@@ -114,17 +157,28 @@ function ZSS:sortrules()
 		elseif r1[4]<r2[4] then return true else                    return false
 		end
 	end)
+
 	return self
 end
 
+-- Given an element descriptor string or table, compute the declarations that apply. For example:
+-- local values = myZSS:match '.ped'
+-- local values = myZSS:match 'text#accel.label'
+-- local values = myZSS:match 'text#accel.value.negative[val=0.3]'
+-- local values = myZSS:match{ type='text', id='accel', tags={value=1, negative=1}, data={value=0.3} }
+--
+-- Using a string will cache the result. Later requests that use the same string are 40x faster.
+--
+-- Use an element descriptor will not use the cache (either for lookup or storing the result).
+-- Use element descriptors when you have data values that change.
 function ZSS:match(el)
-	local signagure
+	local descriptor
 	if type(el)=='string' then
-		signature = el
-		if self.computed[signature] then
-			return self.computed[signature]
+		descriptor = el
+		if self._computed[descriptor] then
+			return self._computed[descriptor]
 		else
-			el = ZSS.parse_selector(signature, true)
+			el = self:parse_selector(descriptor, true)
 		end
 	end
 
@@ -137,13 +191,14 @@ function ZSS:match(el)
 		end
 	end
 
-	if signature then
-		self.computed[signature] = computed
+	if descriptor then
+		self._computed[descriptor] = computed
 	end
 
 	return computed
 end
 
+-- Test to see if an element descriptor table matches a specific selector table
 function ZSS.matches(selector, el)
 	if selector.type and el.type~=selector.type then return false end
 	if selector.id   and el.id~=selector.id     then return false end
